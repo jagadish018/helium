477eed4a85ed55a7b350d96cba49e889ce80c68a
jagadishck018 <jagadishck.018@gmail.com>
2025-03-11 18:15:54 +0530

    add API endpoints for generating random numbers, current time, and managing reminders

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,64 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const reminders: string[] = [];
+
+app.get("/generate", (context) => {
+  return context.json({ randomNumber: Math.random()*100 });
+});
+
+app.get("/current-time", (c) => {
+  const date = new Date();
+  return c.json({ currentTime : date.toISOString() });
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/h", (c) => {
+  const nodeVersion = process.version;
+  const platform = process.platform;
+  return c.json({
+    nodeVersion: nodeVersion,
+    platform: platform,
+  });
+
+});  
+
+
+app.get("/puppet", (c) => {
+
+  const qp = c.req.query(); 
+  return c.json(qp,200);
+});
+
+app.post("/reminders", async (c) => {
+  const body = await c.req.json();
+  const reminder = body.reminder;
+  reminders.push(reminder);
+  return c.json(reminders, 201);
+});
+
+
+const numbers: number[] = [];
+
+
+app.post("/numbers", async (c) => {
+  const randomNumber = await c.req.json();
+  const number = randomNumber.number;
+  numbers.push(number);
+  return c.json({lastStorednumber: number}, 201);
+});
+
+app.get("/numbers", (c) => {
+  
+  return c.json({numbers},200);
+});
+
+
+
+
+
+
+
+serve(app);
+console.log("Server is running on port http://localhost:3000/health");
