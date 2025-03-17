c920f3be74427004c57918b63657944761e88699
jagadishck018 <jagadishck.018@gmail.com>
2025-03-17 19:17:41 +0530

    refactor API endpoints for improved structure and consistency

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,75 @@
-import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
 const app = new Hono();
 
-const reminders: string[] = [];
+serve(app);
 
 app.get("/generate", (context) => {
-  return context.json({ randomNumber: Math.random()*100 });
+  return context.json(
+    {
+      randomNumber: Math.random(),
+    },
+    200
+  );
 });
 
-app.get("/current-time", (c) => {
+app.get("/current-time", (context) => {
   const date = new Date();
-  return c.json({ currentTime : date.toISOString() });
-})
 
-app.get("/h", (c) => {
-  const nodeVersion = process.version;
-  const platform = process.platform;
-  return c.json({
-    nodeVersion: nodeVersion,
-    platform: platform,
-  });
-
-});  
-
-
-app.get("/puppet", (c) => {
-
-  const qp = c.req.query(); 
-  return c.json(qp,200);
+  return context.json(
+    {
+      currentTime: date.toLocaleString(),
+    },
+    200
+  );
 });
 
-app.post("/reminders", async (c) => {
-  const body = await c.req.json();
-  const reminder = body.reminder;
-  reminders.push(reminder);
-  return c.json(reminders, 201);
+app.get("/environment", (context) => {
+  const currentNodeVersion = process.version;
+  const currentPlatform = process.platform;
+
+  return context.json(
+    {
+      version: currentNodeVersion,
+      platform: currentPlatform,
+    },
+    200
+  );
 });
 
+app.get("/puppet", (context) => {
+  const queryParameters = context.req.query();
+
+  return context.json(
+    {
+      queryParameters,
+    },
+    200
+  );
+});
 
 const numbers: number[] = [];
 
+app.post("/numbers", async (context) => {
+  const body = await context.req.json();
+  const number = body.number;
 
-app.post("/numbers", async (c) => {
-  const randomNumber = await c.req.json();
-  const number = randomNumber.number;
   numbers.push(number);
-  return c.json({lastStorednumber: number}, 201);
+
+  return context.json(
+    {
+      storedNumber: number,
+    },
+    200
+  );
 });
 
-app.get("/numbers", (c) => {
-  
-  return c.json({numbers},200);
+app.get("/numbers", (context) => {
+  return context.json(
+    {
+      numbers,
+    },
+    200
+  );
 });
-
-
-
-
-
-
-
-serve(app);
-console.log("Server is running on port http://localhost:3000/health");
